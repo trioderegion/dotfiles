0a2ac4cb32d2818f54d305016a8f56d5f56147b6
trioderegion <matt.haentschke@gmail.com>
2020-01-16 13:14:53 -0500

    Periodic Update of used DotFiles

--- vim/vimmerge.cmd
+++ vim/vimmerge.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Vim\vim74\gvim.exe" +"set lines=999" +"set columns=9999" +"simalt ~x" +"wincmd =" +"wincmd w" +"normal gg]C" -d %*
+"C:\Program Files (x86)\Vim\vim80\gvim.exe" +"set lines=999" +"set columns=9999" +"simalt ~x" +"wincmd =" +"wincmd w" +"normal gg]C" -d %*
